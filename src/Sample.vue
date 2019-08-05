7a85fe3372dc6995e752298f10a26a5b7f800076
oota <shark747474@gmail.com>
2019-08-05 16:08:15 +0900

    add first component

--- src/Sample.vue
+++ src/Sample.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <v-app>
     <v-toolbar app>
@@ -6,13 +8,17 @@
       </v-toolbar-title>
     </v-toolbar>
     <img alt="Vue logo" :src="image_src">
+    <Header />
   </v-app>
-
 </template>
 
 
 <script>
+import Header from "./Header.vue";
 export default {
+  components: {
+    Header
+  },
   data () {
       return {
           image_src: require("./images/logo.png") // ←
--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import Sample from "./Sample.vue"
 
 Vue.config.productionTip = false
 
+
 new Vue({
   Vuetify,
   render: h => h(Sample),
 }).$mount('#app')
+
+/*
+new Vue({
+  Vuetify,
+  el: "#app",
+  components: {Sample},
+  template: "<Sample/>"
+});
+*/
