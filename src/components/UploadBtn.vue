42a3d9dba3b31cceb2d85a746dc8d37428e65ae4
oota <shark747474@gmail.com>
2019-08-08 12:56:14 +0900

    show uploaded image func

--- src/components/UploadBtn.vue
+++ src/components/UploadBtn.vue
@@ -6,7 +6,8 @@
 -->
   <v-flex xs12 class="upload-form">
     <v-btn v-on:click="inputClick" rounded small color="warning" class="select-btn" style="font-weight: bold;">画像をアップロード</v-btn>
-    <input class="form-button" type="file" name="file-submit" id="upload-btn"/>
+    <input class="form-button" type="file" name="file-submit" id="upload-btn" v-on:change="uploadImageFile"/>
+    <img v-show="uploadedImage" class="preview-item-file" :src="uploadedImage" alt="" />
   </v-flex>
 </template>
 
@@ -14,11 +15,25 @@
 export default {
   name: "UploadBtn",
   data: () => ({
+    uploadedImage: '',
+    img_name: '',
   }),
   methods: {
     inputClick: function() {
       document.getElementById("upload-btn").click();
-    }
+    },
+    uploadImageFile(e) {
+      const files = e.target.files;
+      this.createImage(files[0]);
+      this.img_name = files[0].name;
+    },
+    createImage(file) {
+      const reader = new FileReader();
+      reader.onload = e => {
+        this.uploadedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
   }
 };
 </script>
