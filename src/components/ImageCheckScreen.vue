18316bbb6a4eccd54d07eee9e02d54443478186d
oota <shark747474@gmail.com>
2019-08-10 01:34:50 +0900

    remove wasted code

--- src/components/ImageCheckScreen.vue
+++ src/components/ImageCheckScreen.vue
@@ -1,56 +1,5 @@
 <template>
-<!--
-  <v-container fluid fill-height justify-center style="height: 500px;">
-    <v-layout wrap class="align-center justify-center row">
-      <v-flex xs12 sm10 class="check-screen-container">
 
-
-        <v-container fluid fill-height>
-          <v-layout wrap>
-            <v-flex xs12 sm10 class="img-bar-container">
-
-
-              <v-container fluid fill-height>
-                <v-layout wrap>
-                  <v-flex xs12 sm8 class="img-container">
-                    <img v-show="imgUrl" v-bind:src="imgUrl" class="display-img"/>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-
-
-              <v-container fluid fill-height>
-                <v-layout wrap>
-                  <v-flex xs12 sm2 class="bar-container">
-                    <p>bar</p>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-
-
-            </v-flex>
-          <v-flex xs12 sm10 class="mark-text-container">
-            <v-layout wrap>
-              <v-flex xs2 sm2 class="mark-container">
-                <p>mark</p>
-              </v-flex>
-            </v-layout>
-            <v-layout wrap>
-              <v-flex xs8 sm8 class="text-container">
-                <p>text</p>
-              </v-flex>
-            </v-layout>
-
-
-          </v-flex>
-        </v-layout>
-      </v-container>
-
-
-      </v-flex>
-    </v-layout>
-  </v-container>
--->
 <v-container fluid fill-height justify-center>
   <v-layout wrap class="align-center justify-center row">
     <v-flex xs12 sm10 md8 lg6 xl4>
@@ -115,6 +64,7 @@
     </v-flex>
   </v-layout>
 </v-container>
+
 </template>
 
 <script>
@@ -180,22 +130,5 @@ export default {
   height: 80%;
   border: 1px solid;
 }
-/*
-.check-screen-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.img-bar-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.mark-text-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-*/
+
 </style>
