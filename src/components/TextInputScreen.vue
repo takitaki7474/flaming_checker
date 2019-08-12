6bfa7d22544612a9f0b582b967becbbca79c935b
oota <shark747474@gmail.com>
2019-08-12 14:00:52 +0900

    set textarea

--- src/components/TextInputScreen.vue
+++ src/components/TextInputScreen.vue
@@ -3,7 +3,13 @@
 <v-container fluid fill-height justify-center>
   <v-layout wrap class="align-center justify-center row">
     <v-flex xs12 sm10 md8 lg6 xl4>
-      <p>inputTextScreenです</p>
+      <v-textarea color="teal">
+         <template v-slot:label>
+           <div>
+             Bio <small>(optional)</small>
+           </div>
+         </template>
+       </v-textarea>
     </v-flex>
   </v-layout>
 </v-container>
