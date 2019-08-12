a359fa84778be86febf65ce6f781a86ec008659b
oota <shark747474@gmail.com>
2019-08-12 14:20:07 +0900

    change textarea size

--- src/components/TextInputScreen.vue
+++ src/components/TextInputScreen.vue
@@ -3,10 +3,10 @@
 <v-container fluid fill-height justify-center>
   <v-layout wrap class="align-center justify-center row">
     <v-flex xs12 sm10 md8 lg6 xl4>
-      <v-textarea color="teal">
+      <v-textarea color="teal" rows="20">
          <template v-slot:label>
            <div>
-             Bio <small>(optional)</small>
+             投稿を入力してください
            </div>
          </template>
        </v-textarea>
